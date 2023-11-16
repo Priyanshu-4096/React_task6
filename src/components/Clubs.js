9a3e7139b64aab5e31b7633d972f220b678b27ff
Priyanshu Joarder <priyanshujoarder16@gmail.com>
2023-11-16 23:32:08 +0530

    commit

---
+++ src/components/Clubs.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Clubs = () => {
+  return (
+    <section>
+      <h2>Clubs & Organizations</h2>
+      <ul>
+        <li>IETE</li>
+        <li>IEEE - RAS</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Clubs;
---
+++ src/components/Technicalskills.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Technicalskills = () => {
+  return (
+    <section>
+      <h2>Technical Skills</h2>
+      <ul>
+        <li>Android Development</li>
+        <li>Web Development: HTML, CSS, Bootstrap, JavaScript</li>
+        <li>Programming Languages: Python, Java, C, C++</li>
+        <li>Database Management: SQL</li>
+        <li>Frameworks and Libraries: React, Node.js</li>
+        <li>Data Structures</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Technicalskills;
